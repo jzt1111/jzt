29909231eec5c2bfd69de44d5b5d1a4dfe361b08
jzt1111 <1802188077@qq.com>
2022-05-22 20:43:17 +0800

    111

---
+++ config.js
@@ -0,0 +1,24 @@
+var config = {
+	
+	'url': 'https://api.it120.cc/',
+	
+	
+	'subDomain': 'fireshop',
+	
+	
+	'version': '1.0.0',
+	
+	'closeorder': '60', 
+	
+	'closeorderkey': 'ihjZ2LiMQUH-G9UR9B2TDI0xQWRb0m4IT5_8s1nbZS0',
+	
+	
+	'deliveryorderkey': 'HXtRlV3djH9MFVOm_kjMQv4GLXC4q7EdyUc9XUzOEgk',
+	
+	
+	'assessorderkey': 'SbpRcF3FQnK9qIieYTHpQYYvuar6xOqVfs_6bNuxtlw',
+	
+	
+	'successorderkey': 'uySaxE9mAJYTvsshRibxSLCxFA1beXXf-USc7ftD_pA'
+}
+module.exports = config
